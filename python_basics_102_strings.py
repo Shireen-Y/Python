dea036b6c6a76b17ba9a4664fb90a5401b1f7f77
Shireen Yawer <SYawer@spartaglobal.com>
2019-12-02 14:08:24 +0000

    completing strings and useful methods

--- python_basics_102_strings.py
+++ python_basics_102_strings.py
@@ -28,4 +28,33 @@ print ('Welcome <person>, you were born on the year <date_birth>')
 print (f'Welcome {name}, you are {age} old')
 print (f'Welcome {name}, you were born on the year {2019 - age}')
 
+
 ## Useful methods for strings
+example_string = " HELLoo "
+print(example_string)
+print(example_string.strip()) # Remove trailing white spaces
+print(example_string.count('L')) # Count number of characters in the string
+print(example_string.lower())
+print(example_string.upper())
+print(example_string.strip().capitalize()) # Chaining methods
+
+# Learning and using .split()
+text_to_split = 'this is some example text in our file'
+results_split = text_to_split.split(' ')
+print(results_split)
+
+# Standard builtin function len()
+print(len(example_string))
+
+
+# Casting int
+str_string = '1990'
+print(type(str_string))
+
+## str --> int
+int_number = int(str_string)
+print(type(int_number))
+
+## int --> str
+new_str = str(int_number)
+print (type(new_str))

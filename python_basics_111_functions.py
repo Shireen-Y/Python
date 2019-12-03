b426dbb16928b9993284ffe5bed649a361c2ad1c
Shireen Yawer <SYawer@spartaglobal.com>
2019-12-03 14:56:25 +0000

    exercises

---
+++ python_basics_111_functions.py
@@ -0,0 +1,41 @@
+# Functions
+
+# A function is like a machine
+# It needs to be turned on (called) and it does things (blocks of code)
+
+# Good practices
+# They should be small and only do one job
+    # Avoid stringy code
+    # Testable code
+    # Maintainable code
+    # Readable code
+
+# DO NOT PRINT inside a function - unless it explicitly suppose to print
+# In function you return
+# Call the function inside a print(function_name)
+# DRY --> Don't Repeat Yourself
+# Separation of concerns
+    # Define functions in one file
+    # Call them in another
+    # There is more to this
+
+# Functions work this way:
+    # Define a function
+    # Call the function
+
+# Syntax
+# def <name>(arg1, arg2, arg3):
+    # Block
+    # Return something if you wish
+
+def say_hello():
+    return 'HELLOO'
+
+print(say_hello())
+
+def say_hello_ueser(f_name = 'Shireen', l_name = 'Yawer'):
+    return 'Hello ' + f_name.capitalize() + ' ' + l_name.capitalize()
+
+print(say_hello_ueser('Shireen', 'Yawer'))
+
+
